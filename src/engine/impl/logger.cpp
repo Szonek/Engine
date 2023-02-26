b6e9264996c88c14a5df6909cdb6b349c6e08bf1
szonek <szymonpl17@gmail.com>
2023-02-26 09:47:08 +0100

    android log but need to debug it

--- src/engine/impl/logger.cpp
+++ src/engine/impl/logger.cpp
@@ -1,15 +1,14 @@
 #include "logger.h"
 
 #include <chrono>
-#include <sstream>
 #include <unordered_map>
 
-#if __ANDROID__
-#error Impl here
-#else
 #include <fmt/format.h>
-#include <fmt/color.h>
 #include <fmt/chrono.h>
+#include <fmt/color.h>
+
+#if __ANDROID__
+#include <android/log.h>
 #endif
 
 struct LogLevelTraits
@@ -42,19 +41,10 @@ inline Stream& streamin_with_braces(Stream& s, const T& t)
 void engine::log::log(LogLevel level, std::string_view msg)
 {
     const auto log_level_trait = LOG_LEVEL_LUT[level];
-
-    //const auto system_clock_now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    //const auto local_time = std::localtime(&system_clock_now);
-    //
-    //std::stringstream stream;
-    //streamin_with_braces(stream, std::put_time(local_time, "%Y-%m-%d %X")) << " ";
-    //streamin_with_braces(stream, LOG_LEVEL_LUT[level]) << " ";
-    //streamin(stream, msg) << "\n";
-
     const auto printable_str = fmt::format("[{}][{}]: {}", std::chrono::system_clock::now(), log_level_trait.name, msg);
 
 #if __ANDROID__
-#error Impl here
+    __android_log_print(ANDROID_LOG_ERROR, "", "%s", printable_str.data());
 #else
     fmt::print(fmt::fg(log_level_trait.color), printable_str);
 #endif
