6aca76fe26ea67d7ff8bb1713c8819fbeb8392e9
szonek <szymonpl17@gmail.com>
2024-05-11 09:47:58 +0200

    camera follow character

--- src/games/project_c/camera_script.h
+++ src/games/project_c/camera_script.h
@@ -7,47 +7,6 @@
 namespace project_c
 {
 
-class CameraScript : public engine::IScript
-{
-public:
-    CameraScript(engine::IScene* my_scene)
-        : IScript(my_scene)
-    {
-        const auto scene = my_scene_->get_handle();
-        const auto app = my_scene_->get_app_handle();
-        auto nc = engineSceneAddNameComponent(scene, go_);
-        std::strncpy(nc.name, "camera", strlen("camera"));
-        engineSceneUpdateNameComponent(scene, go_, &nc);
 
-        auto camera_comp = engineSceneAddCameraComponent(scene, go_);
-        camera_comp.enabled = true;
-        camera_comp.clip_plane_near = 0.1f;
-        camera_comp.clip_plane_far = 1000.0f;
-        camera_comp.type = ENGINE_CAMERA_PROJECTION_TYPE_PERSPECTIVE;
-        camera_comp.type_union.perspective_fov = 45.0f;
-
-        camera_comp.target[0] = 0.0f;
-        camera_comp.target[1] = 0.0f;
-        camera_comp.target[2] = 0.0f;
-
-        engineSceneUpdateCameraComponent(scene, go_, &camera_comp);
-
-        auto camera_transform_comp = engineSceneAddTransformComponent(scene, go_);
-        camera_transform_comp.position[0] = -2.273151f;
-        camera_transform_comp.position[1] = 4.3652916f;
-        camera_transform_comp.position[2] = 1.3330482f;
-        engineSceneUpdateTransformComponent(scene, go_, &camera_transform_comp);
-    }
-
-    void update(float dt) override
-    {
-        const auto scene = my_scene_->get_handle();
-        const auto app = my_scene_->get_app_handle();
-        const auto mouse_coords = engineApplicationGetMouseCoords(app);
-
-        const float move_speed = 1.0f * dt;
-
-    }
-};
 
 }  // namespace project_c
