ff82e6cdd707ea1de36a812481b55b0613ae76db
szonek <szymonpl17@gmail.com>
2025-07-01 20:08:25 +0200

    animation c++ class

---
+++ src/engine/impl/animation_controller.cpp
@@ -0,0 +1,81 @@
+#include "animation_controller.h"
+#include "skin.h"
+#include "logger.h"
+
+#include "ozz/animation/offline/animation_builder.h"
+#include "ozz/animation/offline/raw_animation.h"
+#include "ozz/animation/runtime/skeleton_utils.h"
+
+#include <stdexcept>
+#include <format>
+
+engine::AnimationController::AnimationController(ozz::animation::Skeleton* skeleton)
+    : skeleton_(skeleton)
+{
+if (!skeleton)
+{
+    throw std::invalid_argument("Skeleton pointer cannot be null!");
+}
+}
+
+void engine::AnimationController::add_animation(const AnimationClipDesc& animation_clip)
+{
+    ozz::animation::offline::RawAnimation raw_animation;
+    raw_animation.name = animation_clip.name;
+    raw_animation.duration = animation_clip.duration;
+    raw_animation.tracks.resize(skeleton_->num_joints());
+
+    for (const auto& channel : animation_clip.channels)
+    {
+        const auto joint_idx = ozz::animation::FindJoint(*skeleton_, channel.joint_name.c_str());
+        auto& track = raw_animation.tracks[joint_idx];
+        
+        switch (channel.type)
+        {
+            case AnimationChannelType::eTranslation:
+            {
+                for (std::size_t i = 0; i < channel.timestamps.size(); ++i)
+                {
+                    ozz::animation::offline::RawAnimation::TranslationKey key;
+                    key.time = channel.timestamps.at(i);
+                    key.value = ozz::math::Float3(channel.data.at(i * 3), channel.data.at(i * 3 + 1), channel.data.at(i * 3 + 2));
+                    track.translations.push_back(std::move(key));
+                }
+                break;
+            }
+            case AnimationChannelType::eRotation:
+            {
+                for (std::size_t i = 0; i < channel.timestamps.size(); ++i)
+                {
+                    ozz::animation::offline::RawAnimation::RotationKey key;
+                    key.time = channel.timestamps.at(i);
+                    key.value = ozz::math::Quaternion(channel.data.at(i * 4), channel.data.at(i * 4 + 1), channel.data.at(i * 4 + 2), channel.data.at(i * 4 + 3));
+                    track.rotations.push_back(std::move(key));
+                }
+                break;
+            }
+            case AnimationChannelType::eScale:
+            {
+                for (std::size_t i = 0; i < channel.timestamps.size(); ++i)
+                {
+                    ozz::animation::offline::RawAnimation::ScaleKey key;
+                    key.time = channel.timestamps.at(i);
+                    key.value = ozz::math::Float3(channel.data.at(i * 3), channel.data.at(i * 3 + 1), channel.data.at(i * 3 + 2));
+                    track.scales.push_back(std::move(key));
+                }
+                break;
+            }
+        }
+    }
+
+    if (!raw_animation.Validate())
+    {
+        throw std::runtime_error("Invalid raw animation data.");
+    }
+    const auto num_tracks = raw_animation.tracks.size();
+    log::log(log::LogLevel::eTrace, std::format("Adding animation '{}' with {} tracks to controller.\n", raw_animation.name, num_tracks).c_str());
+
+    ozz::animation::offline::AnimationBuilder builder;
+    ozz::unique_ptr<ozz::animation::Animation> animation = builder(raw_animation);
+    animations_.emplace(raw_animation.name, std::move(animation));
+}
---
+++ src/engine/impl/animation_controller.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "mesh_defs.h"
+
+#include "ozz/animation/runtime/skeleton.h"
+#include "ozz/animation/runtime/animation.h"
+#include "ozz/base/memory/unique_ptr.h"
+
+#include <unordered_map>
+
+namespace engine
+{
+class AnimationController
+{
+public:
+    AnimationController(ozz::animation::Skeleton* skeleton);
+
+    void add_animation(const AnimationClipDesc& animation_clip);
+
+private:
+    ozz::animation::Skeleton* skeleton_ = nullptr;
+    std::unordered_map<std::string, ozz::unique_ptr<ozz::animation::Animation>> animations_;
+};
+
+} // namespace engine
